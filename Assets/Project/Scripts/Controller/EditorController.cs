82b0bc8d96710bdb4acbdf5cbae1e8047816dab4
PoPo <dltngus7535@gmail.com>
2025-05-14 10:01:30 +0900

    에디터 작업

--- Assets/Project/Scripts/Controller/EditorController.cs
+++ Assets/Project/Scripts/Controller/EditorController.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
+using Project.Scripts.Data_Script;
 using TMPro;
 using UnityEditor;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 using Toggle = UnityEngine.UI.Toggle;
 #if UNITY_EDITOR
 using UnityEngine;
@@ -15,6 +18,7 @@ public class EditorController : MonoBehaviour
     public ScrollerItem itemPrefab;
     public RectTransform content;
     public Toggle toggle;
+    public Button createButton;
     public StageData curStage;
     void Awake()
     {
@@ -25,7 +29,7 @@ public class EditorController : MonoBehaviour
             .ToList();
 #endif
     }
-    void Start()
+    async void Start()
     {
         for (int i = 0; i < stages.Count; i++)
             AddItem(stages[i] );
@@ -35,8 +39,36 @@ public class EditorController : MonoBehaviour
             if(null != curStage)
                 await BoardController.Instance.LoadStage(curStage);
         });
+        createButton.onClick.AddListener(()=>
+        {
+#if UNITY_EDITOR
+            StageData data = ScriptableObject.CreateInstance<StageData>();
+            DataInit(data, 3, 3);
+            AssetDatabase.CreateAsset(data,
+                $"Assets/Project/Resource/Data/StageData SO/StageData_{stages.Count + 1}.asset");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            
+            AddItem(data);
+#endif
+        });
     }
 
+    private void DataInit(StageData data, int xSize, int ySize)
+    {
+        data.boardBlocks = new List<BoardBlockData>();
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                data.boardBlocks.Add(new BoardBlockData()
+                {   
+                    x = x,
+                    y = y,
+                });
+            }            
+        }
+    }
 
     private void AddItem(StageData data)
     {
